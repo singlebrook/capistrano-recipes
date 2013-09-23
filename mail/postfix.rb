7089221ff49448beb2f8731146c21f414dced210
Isaiah Fischer <isaiah@singlebrook.com>
2013-09-23 09:15:27 -0500

    All server types can benefit from being able to send mail
    As any server could have a cron job that would need to notify
    developers of a failure to complete it's task.

--- mail/postfix.rb
+++ mail/postfix.rb
@@ -1,6 +1,6 @@
 namespace :postfix do
   desc "Install postfix MTA"
-  task :install, :roles => :app do
+  task :install do
     apt_install 'postfix'
   end
   after "deploy:install", "postfix:install"
