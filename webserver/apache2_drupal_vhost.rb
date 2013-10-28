5fc0b983816fa164f3ad80bb7d608cd7cafd4a12
Leon Miller-Out <leon@singlebrook.com>
2013-10-28 12:38:06 -0400

    Enable mod_rewrite for Drupal

--- webserver/apache2_drupal_vhost.rb
+++ webserver/apache2_drupal_vhost.rb
@@ -1,6 +1,7 @@
 namespace :apache2_drupal_vhost do
   desc "Create a Drupal-ready apache2 vhost for [application]"
   task :install do
+    sudo "a2enmod rewrite"
     put_as_root read_template('apache2_drupal_vhost.erb'), "/etc/apache2/sites-available/#{application}"
     sudo "a2ensite #{application}"
     sudo "service apache2 restart"
