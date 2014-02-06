430ecfc8f98217fcd6dd2ffda525efcf71cb8c84
Leon Miller-Out <leon@singlebrook.com>
2014-02-06 14:15:55 -0500

    Add some common Passenger configuration options

--- appserver/passenger.rb
+++ appserver/passenger.rb
@@ -6,8 +6,21 @@ namespace :passenger do
     sudo 'passenger-install-apache2-module --auto'
     sudo 'passenger-install-apache2-module --snippet > /tmp/passenger.load'
     sudo 'mv /tmp/passenger.load /etc/apache2/mods-available/passenger.load'
+    put_as_root passenger_conf, '/etc/apache2/mods-available/passenger.conf'
     sudo 'a2enmod passenger'
     sudo 'service apache2 restart'
   end
   after "deploy:install", "passenger:install"
+end
+
+
+def passenger_conf
+<<-EOF
+# Always keep instance one of each app running
+PassengerMinInstances 1
+# Don't shut idle instances down
+PassengerPoolIdleTime 0
+# WARNING: This can break other Apache modules
+PassengerHighPerformance on
+EOF
 end
