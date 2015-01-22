c0eae86872bde386d5a84b7997da285e571ca444
Leon Miller-Out <leon@singlebrook.com>
2015-01-22 12:19:14 -0500

    Correct names of apache vhost config file.

--- webserver/apache2_drupal_vhost.rb
+++ webserver/apache2_drupal_vhost.rb
@@ -2,7 +2,7 @@ namespace :apache2_drupal_vhost do
   desc "Create a Drupal-ready apache2 vhost for [application]"
   task :install do
     sudo "a2enmod rewrite"
-    put_as_root read_template('apache2_drupal_vhost.erb'), "/etc/apache2/sites-available/#{application}"
+    put_as_root read_template('apache2_drupal_vhost.erb'), "/etc/apache2/sites-available/#{application}.conf"
     sudo "a2ensite #{application}"
     sudo "service apache2 restart"
   end
--- webserver/apache2_passenger_vhost.rb
+++ webserver/apache2_passenger_vhost.rb
@@ -1,7 +1,7 @@
 namespace :apache2_passenger_vhost do
   desc "Create a Passenger-ready apache2 vhost for [application]"
   task :install do
-    put_as_root read_template('apache2_passenger_vhost.erb'), "/etc/apache2/sites-available/#{application}"
+    put_as_root read_template('apache2_passenger_vhost.erb'), "/etc/apache2/sites-available/#{application}.conf"
     sudo "a2ensite #{application}"
     sudo "service apache2 restart"
   end
